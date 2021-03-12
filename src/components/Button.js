18fd70b1bfc5ce6d7b79e063cd0addab5df6345b
Roayacom <77238762+Roayacom@users.noreply.github.com>
2021-03-12 05:46:19 +0300

    initial commit2

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const Button  =({
 {
    
 return(
-    <button type="submit" class=" bg-gray-700 border-2 border-pink-300 text-pink-300 font-bold lg:text-sm md:text-xs text-xs rounded  px-4 py-3 my-2 hover:bg-gray-800 hover:text-pink-400 hover:border-pink-400">{children}</button>   
+    <button type="submit" class=" bg-gray-700 border-2 border-pink-600 text-pink-600 font-bold lg:text-sm md:text-xs text-xs rounded  px-4 py-3 my-2 hover:bg-gray-800 hover:text-pink-700 hover:border-pink-700">{children}</button>   
 )
 }
 export default Button
